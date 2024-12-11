089a7cbdf99ea3e79bea0fe0e9a7347529e1ac63
Sangeerthan21 <kandeepann5@gmail.com>
2024-12-11 19:04:37 +0530

    python odd or even

---
+++ activity.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+remainder = (num%2)
+
+if(remainder > 0):
+    print("The number you entered is an odd number.")
+else:
+    print("The number you entered is an even number.")
